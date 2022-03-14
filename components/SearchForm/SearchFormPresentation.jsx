851e10e50b955ec9df58a95431c0d526812adead
Miyuusa <74092547+whooolemilk@users.noreply.github.com>
2022-03-14 15:07:00 +0900

    検索バーのtailwind修正 (#27)
    Co-authored-by: wholemilk <wholemilk25@gmail.com>

--- components/SearchForm/SearchFormPresentation.jsx
+++ components/SearchForm/SearchFormPresentation.jsx
@@ -1,23 +1,14 @@
 import IconSearch from "../Icons/IconSearch";
 const SearchFormPresentation = () => {
   return (
-    <form
-      id="form1"
-      action="/result"
-      method="get"
-      className="relative screen mx-4 my-3"
-    >
+    <form action="/result" method="get" className="flex screen mx-4 my-3">
       <input
-        id="sbox1"
         name="query"
         type="search"
         placeholder="料理名、材料名を入力"
-        className="absolute top-0 left-4/6 py-3 pl-7 w-5/6 bg-ocher-100 placeholder-gray-600 rounded-l-full focus:outline-none focus:border-none focus:ring-none focus:placeholder-opacity-0"
+        className="py-3 pl-7 w-5/6 bg-ocher-100 placeholder-gray-600 rounded-l-full focus:outline-none focus:border-none focus:ring-none focus:placeholder-opacity-0"
       />
-      <button
-        type="submit"
-        className="absolute top-0 right-0 w-1/6 py-3 bg-ocher-100  rounded-r-full"
-      >
+      <button type="submit" className="w-1/6 py-3 bg-ocher-100 rounded-r-full">
         <IconSearch />
       </button>
     </form>
