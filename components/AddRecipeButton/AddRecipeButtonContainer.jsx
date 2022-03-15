33d9ab06402d82e7d7330251541caac613a1c045
wholemilk <wholemilk25@gmail.com>
2022-03-15 18:59:16 +0900

    AddRecipeButtonの修正（でも動かない…）

--- components/AddRecipeButton/AddRecipeButtonContainer.jsx
+++ components/AddRecipeButton/AddRecipeButtonContainer.jsx
@@ -1,33 +1,28 @@
 import AddRecipeButtonPresentation from "./AddRecipeButtonPresentation";
-import { useState } from "react/cjs/react.production.min";
+import { useState } from "react";
 
 const AddRecipeButtonContainer = ({ list }) => {
   const recipe = list[0];
-  const key = recipe.recipeId;
+  const recipeId = recipe.recipeId;
   const menuData = recipe.recipeMaterial;
 
-  // const [flag, setFlag] = useState(false);
-  // const submit = () => {
-  //   if (!flag) {
-  //     localStorage.setItem(recipeId, JSON.stringify(menuData));
-  //     setFlag(true);
-  //   } else {
-  //     setFlag(false);
-  //     localStorage.removeItem(recipeId);
-  //   }
-  // };
+  const [flag, setFlag] = useState(false);
+  const submit = () => {
+    if (!flag) {
+      localStorage.setItem(recipeId, JSON.stringify(menuData));
+      setFlag(true);
+    } else {
+      setFlag(false);
+      localStorage.removeItem(recipeId);
+    }
+  };
 
-  // const handleClick = () => {
-  //   submit();
-  // };
+  const handleClick = () => {
+    submit();
+  };
 
   return (
-    <AddRecipeButtonPresentation
-      recipeId={key}
-      menuData={menuData}
-      // flag={flag}
-      // onClick={() => handleClick()}
-    />
+    <AddRecipeButtonPresentation flag={flag} onClick={() => handleClick()} />
   );
 };
 
