95bfe1d7df207f5adc8da12df997dbad59ffbff7
wholemilk <wholemilk25@gmail.com>
2022-03-24 02:22:57 +0900

    ヘッダーフッターのcss修正

--- components/Icons/IconInvalidSearch.jsx
+++ components/Icons/IconInvalidSearch.jsx
@@ -3,7 +3,7 @@ const IconInvalidSearch = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-12 w-auto stroke-gray-400 hover:stroke-gray-400 "
+      className="h-8 w-auto stroke-gray-400 hover:stroke-gray-400 "
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
--- components/Icons/IconValidSearch.jsx
+++ components/Icons/IconValidSearch.jsx
@@ -3,7 +3,7 @@ const IconValidSearch = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-12 w-auto stroke-ocher-400 hover:stroke-ocher-300 "
+      className="h-8 w-auto stroke-ocher-400 hover:stroke-ocher-300 "
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
