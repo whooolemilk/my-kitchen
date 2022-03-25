2c7ea02da8dc69a64bf4e3185f8c32955e341e7b
wholemilk <wholemilk25@gmail.com>
2022-03-26 02:53:14 +0900

    つくるページを作成

--- components/Menu/MenuPresentation.jsx
+++ components/Menu/MenuPresentation.jsx
@@ -1,40 +1,42 @@
 import Image from "next/dist/client/image";
 import Link from "next/dist/client/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import IconClock from "../Icons/IconClock";
 import IconYen from "../Icons/IconYen";
 
 const MenuPresentation = ({ keysList, materialsList }) => {
   //console.log(keysList);
+  const [isVisible, setIsVisible] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  let idList = [];
+
+  const handleClick = () => {
+    setIsVisible(!isVisible);
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       // レシピデータをとってくる
       // keyListはレシピのidが複数入ってるリスト
       for (let i = 0; i < keysList.length; i++) {
         const data = await fetch(
-          "https://forked-mykitchen-backend.herokuapp.com/recipes/" + keyList[i]
+          "https://forked-mykitchen-backend.herokuapp.com/recipes/" +
+            keysList[i]
         );
         const json = await data.json();
-        console.log(data); // response
-        console.log(json); // 空
-
-        //idList.push(json);
+        idList.push(json);
       }
-
-      //setList(idList);
-      //console.log(idList);
+      setRecipeList(idList);
     };
-
     fetchData().catch(console.error);
-  }, []);
+  }, [keysList.length]);
+  console.log(recipeList);
   return (
     <>
-      {/* <h1 className="text-xl mx-4">
-        <span className="font-bold"></span>の検索結果
-      </h1>
+      <h1 className="text-xl mx-4">つくる</h1>
       <ul className="divide-y divide-gray-300">
-        {list.map((recipe, i) => (
-          <li key={i} className=" ">
+        {recipeList.map((recipe, i) => (
+          <li key={i}>
             <Link
               href={{
                 pathname: "/recipe",
@@ -76,9 +78,58 @@ const MenuPresentation = ({ keysList, materialsList }) => {
                 </div>
               </a>
             </Link>
+            <ul>
+              <div className="flex border-y px-4 ">
+                <li className="py-3 flex-grow">材料を確認</li>
+                <button onClick={() => handleClick(isVisible)}>
+                  <li>
+                    {isVisible ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M5 15l7-7 7 7"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 my-3"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M19 9l-7 7-7-7"
+                        />
+                      </svg>
+                    )}
+                  </li>
+                </button>
+              </div>
+              {isVisible ? (
+                recipe.materials.map((material, i) => (
+                  <li key={i} className="px-8 py-3 bg-white odd:bg-ocher-100">
+                    {material}
+                  </li>
+                ))
+              ) : (
+                <></>
+              )}
+            </ul>
           </li>
         ))}
-      </ul> */}
+      </ul>
     </>
   );
 };
