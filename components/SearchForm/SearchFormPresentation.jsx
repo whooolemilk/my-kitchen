f0558507e83d9d36c5121f7a0b8059184d4c6157
Miyuusa <74092547+whooolemilk@users.noreply.github.com>
2022-03-25 14:00:24 +0900

    検索した入力が遷移先でも残るようにした (#38)
    Co-authored-by: wholemilk <wholemilk25@gmail.com>

--- components/SearchForm/SearchFormPresentation.jsx
+++ components/SearchForm/SearchFormPresentation.jsx
@@ -1,7 +1,6 @@
 import IconSearch from "../Icons/IconSearch";
-import { useRouter } from "next/router";
-const SearchFormPresentation = () => {
-  const router = useRouter();
+
+const SearchFormPresentation = ({ keyword, onChange }) => {
   return (
     <form action="/result" method="get" className="flex screen mx-4 my-3">
       <input
@@ -9,7 +8,8 @@ const SearchFormPresentation = () => {
         type="search"
         placeholder="料理名、材料名を入力"
         className="py-3 pl-7 w-5/6 bg-ocher-100 placeholder-gray-600 rounded-l-full focus:outline-none focus:border-none focus:ring-none focus:placeholder-opacity-0"
-        value={router.query.keyword}
+        value={keyword}
+        onChange={onChange}
       />
       <button type="submit" className="w-1/6 py-3 bg-ocher-100 rounded-r-full">
         <IconSearch />
