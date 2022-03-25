315a1a0c4e0507d1291370f4b108578d7f2c6b1d
wholemilk <wholemilk25@gmail.com>
2022-03-25 11:16:58 +0900

    検索した入力が遷移先でも残るようにした

--- components/SearchForm/SearchFormContainer.jsx
+++ components/SearchForm/SearchFormContainer.jsx
@@ -1,7 +1,20 @@
 import SearchFormPresentation from "./SearchFormPresentation";
-
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useEffect } from "react";
 const SearchFormContainer = () => {
-  return <SearchFormPresentation />;
+  const router = useRouter();
+  const [keyword, setKeyword] = useState("");
+  useEffect(() => {
+    if (router.query.keyword && !keyword) {
+      setKeyword(router.query.keyword);
+    }
+  }, [router.query.keyword]);
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  return <SearchFormPresentation keyword={keyword} onChange={handleChange} />;
 };
 
 export default SearchFormContainer;
