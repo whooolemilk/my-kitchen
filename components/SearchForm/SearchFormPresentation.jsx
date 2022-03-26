1fde3f5771e2815b2eb9f0e790970f4e4bbe5482
Miyuusa <74092547+whooolemilk@users.noreply.github.com>
2022-03-26 16:32:17 +0900

    safariで動かないborder-radius修正 (#51)
    * safariで動かないborder-radius修正
    
    * safariで動かないborder-radius修正
    
    * safariで動かないborder-radius修正
    
    * 背景色の変更
    
    Co-authored-by: wholemilk <wholemilk25@gmail.com>

--- components/SearchForm/SearchFormPresentation.jsx
+++ components/SearchForm/SearchFormPresentation.jsx
@@ -2,16 +2,21 @@ import IconSearch from "../Icons/IconSearch";
 
 const SearchFormPresentation = ({ keyword, onChange }) => {
   return (
-    <form action="/result" method="get" className="flex screen mx-4 my-3">
+    <form
+      action="/result"
+      method="get"
+      className="flex screen mx-4 my-3 relative z-10 bg-ocher-100 rounded-full"
+    >
       <input
         name="keyword"
         type="search"
         placeholder="料理名、材料名を入力"
-        className="py-3 pl-7 w-5/6 bg-ocher-100 placeholder-gray-600 rounded-l-full focus:outline-none focus:border-none focus:ring-none focus:placeholder-opacity-0"
+        className="py-3 pl-7 w-5/6 placeholder-gray-600 focus:outline-none focus:border-none focus:ring-none focus:placeholder-opacity-0 bg-transparent"
+        style={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}
         value={keyword}
         onChange={onChange}
       />
-      <button type="submit" className="w-1/6 py-3 bg-ocher-100 rounded-r-full">
+      <button type="submit" className="w-1/6 py-3">
         <IconSearch />
       </button>
     </form>
