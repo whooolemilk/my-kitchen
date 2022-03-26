df25ff05e1556832ffee80107bf59708d124d1fa
wholemilk <wholemilk25@gmail.com>
2022-03-26 14:50:03 +0900

    マイレシピリストに変更

--- components/Menu/MenuPresentation.jsx
+++ components/Menu/MenuPresentation.jsx
@@ -15,7 +15,40 @@ const MenuPresentation = ({ recipeList }) => {
 
   return (
     <>
-      <h1 className="text-xl mx-4">つくる</h1>
+      <div className="flex mx-4 my-3">
+        <h1 className="text-xl flex-grow font-bold">マイレシピリスト</h1>
+        {/* <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 mx-2 my-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 my-1 mx-2"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+          />
+        </svg> */}
+      </div>
+      <p className="mx-4 p-2 bg-ocher-100 rounded-lg">
+        今日作りたい料理や近いうちに作ってみたい料理などを材料メモとともに保存しておくことが出来ます！
+      </p>
       <ul className="divide-y divide-gray-300">
         {recipeList.map((recipe, i) => (
           <li key={i}>
@@ -62,7 +95,7 @@ const MenuPresentation = ({ recipeList }) => {
             </Link>
             <ul>
               <div className="flex border-y px-4 ">
-                <li className="py-3 flex-grow">材料を確認</li>
+                <li className="py-3 flex-grow">材料メモを確認</li>
                 <button onClick={() => handleClick(isVisible)}>
                   <li>{isVisible ? <IconUp /> : <IconDown />}</li>
                 </button>
