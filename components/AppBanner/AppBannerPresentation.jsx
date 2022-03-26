28825de9a3ab38e624e0233642468b90a8bbd6da
wholemilk <wholemilk25@gmail.com>
2022-03-27 01:41:57 +0900

    ホームのバナー画像を作成

--- components/AppBanner/AppBannerPresentation.jsx
+++ components/AppBanner/AppBannerPresentation.jsx
@@ -1,6 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const AppBannerPresentation = () => {
   return (
-    <div className="mx-4 mt-3 overflow-hidden rounded-lg w-auto h-28 bg-yellow-300"></div> // 後で画像に置き換える
+    <Link href="/">
+      <a>
+        <div className="mx-4 my-3">
+          <Image
+            src="/banner.png"
+            width={1792}
+            height={686}
+            alt="banner"
+            className=" rounded-2xl"
+          ></Image>
+        </div>
+      </a>
+    </Link>
   );
 };
 
