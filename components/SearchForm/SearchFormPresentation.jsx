0a184c1f634cc86aae8be95a11b72e0c300f944b
wholemilk <wholemilk25@gmail.com>
2022-03-26 16:13:59 +0900

    safariで動かないborder-radius修正

--- components/SearchForm/SearchFormPresentation.jsx
+++ components/SearchForm/SearchFormPresentation.jsx
@@ -11,7 +11,7 @@ const SearchFormPresentation = ({ keyword, onChange }) => {
         name="keyword"
         type="search"
         placeholder="料理名、材料名を入力"
-        className="py-3 pl-7 w-5/6 bg-ocher-100 placeholder-gray-600 rounded-l-full focus:outline-none focus:border-none focus:ring-none focus:placeholder-opacity-0"
+        className="py-3 pl-7 w-5/6 bg-ocher-100 placeholder-gray-600 rounded-tl-full rounded-bl-full focus:outline-none focus:border-none focus:ring-none focus:placeholder-opacity-0"
         value={keyword}
         onChange={onChange}
       />
