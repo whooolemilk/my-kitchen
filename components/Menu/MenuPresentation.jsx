11b5f12ee5264aabe9ff914f082bfae9cc06af69
wholemilk <wholemilk25@gmail.com>
2022-03-27 17:14:51 +0900

    マイレシピリストが空の時に表示する画像設置

--- components/Menu/MenuPresentation.jsx
+++ components/Menu/MenuPresentation.jsx
@@ -1,4 +1,5 @@
 import MyRecipeItemContainer from "../MyRecipeItem/MyRecipeItemContainer";
+import Image from "next/image";
 
 const MenuPresentation = ({ recipeList }) => {
   return (
@@ -10,9 +11,20 @@ const MenuPresentation = ({ recipeList }) => {
         今日作りたい料理や近いうちに作ってみたい料理などを材料メモとともに保存しておくことが出来ます！
       </p>
       <ul className="divide-y divide-gray-300">
-        {recipeList.map((recipe, i) => (
-          <MyRecipeItemContainer recipe={recipe} key={i} />
-        ))}
+        {recipeList.length === 0 ? (
+          <div className="text-center my-8">
+            <Image
+              src="/empty-myrecipe.png"
+              width={300}
+              height={450}
+              alt="description"
+            ></Image>
+          </div>
+        ) : (
+          recipeList.map((recipe, i) => (
+            <MyRecipeItemContainer recipe={recipe} key={i} />
+          ))
+        )}
       </ul>
     </>
   );
